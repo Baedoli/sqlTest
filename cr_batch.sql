938e9bbb9dffb86b45adec251f26bf2b032c83d0
Baedoli <>
2018-04-30 04:25:53 +0900

    sqldeveloper second...

--- cr_batch.sql
+++ cr_batch.sql
@@ -1,7 +1,18 @@
 create or replace procedure cr_batch
 is
+
+v_ varchar2(10) := null;
+
 begin
 
-    null;
+    if 1=2 then
+        v_ := 'A';
+    else
+        v_ := 'B';
+    end if;    
+    
+    -- test
+    -- test2
+    
 
 end;
